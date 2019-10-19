7586541b8ec1dd162e9de2196e66d5837775cf44
Jordan Peters <rollersteaam@googlemail.com>
2019-10-19 20:28:18 +0100

    Create health bar, obstacle collision, player health

---
+++ lastlight/Assets/Scripts/Gameplay/Actors/Obstacle/Obstacle.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        var player = other.GetComponent<Player>();
+
+        player.Damage(20);
+
+        Destroy(gameObject);
+    }
+}
