3a65c9ca7b35b753d0b2843c8bd7ae04208f4c75
Milo Higgins <mh882@exeter.ac.uk>
2019-10-19 21:49:47 +0100

    Implemented movement and camera tracking

---
+++ lastlight/Assets/CameraFollowPlayer.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class CameraFollowPlayer : MonoBehaviour
+{
+    public Transform player;//set in inspector
+    public Vector3 offset;//set in inspector
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 playerPositionX = new Vector3(player.position.x, 0, 0);
+        transform.position = playerPositionX + offset;
+    }
+}
---
+++ lastlight/Assets/Movement.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public Rigidbody rb;//set in inspector
+
+    public float forwardForce = 200f;
+    public float sidewaysForce = 150f;
+    public float upwardForce = 1000f;
+    public float maxVelocity = 10f;
+    public int depth = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if ( (Input.GetKey(UnityEngine.KeyCode.A)) && (rb.velocity[2] <= maxVelocity) )
+        {
+            rb.AddForce(0,0,sidewaysForce);
+        }
+        if ( (Input.GetKey(UnityEngine.KeyCode.D)) && (rb.velocity[2] >= (-1*maxVelocity)) )
+        {
+            rb.AddForce(0,0,-1 * sidewaysForce);
+        }
+        if ( (Input.GetKey(UnityEngine.KeyCode.W)) && (rb.velocity[0] <= maxVelocity) )
+        {
+            rb.AddForce(forwardForce,0,0);
+        }
+        if ( (Input.GetKey(UnityEngine.KeyCode.S)) && (rb.velocity[0] >= (-1*maxVelocity)) )
+        {
+            rb.AddForce(-1 * forwardForce,0,0);
+        }//ground is at y=0
+        if ((Input.GetKey(UnityEngine.KeyCode.Space)) && rb.position[1] < 0.1 && rb.velocity[1] <= 0)
+        {
+            rb.AddForce(0, 5*upwardForce, 0);
+        }
+    }
+}
