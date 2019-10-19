41fcc96ca8b95028a0be1d8e9267a71a5cb7f3c6
Jordan Peters <rollersteaam@googlemail.com>
2019-10-19 23:49:33 +0100

    Create procedural generation and score system

--- lastlight/Assets/Movement.cs
+++ lastlight/Assets/Movement.cs
@@ -2,7 +2,7 @@
 
 public class Movement : MonoBehaviour
 {
-    Rigidbody rb;//set in inspector
+    Rigidbody rb;
 
     public float forwardForce = 200f;
     public float sidewaysForce = 150f;
@@ -10,34 +10,32 @@ public class Movement : MonoBehaviour
     public float maxVelocity = 10f;
     public int depth = 10;
 
-    // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
     void FixedUpdate()
     {
-        if ( (Input.GetKey(UnityEngine.KeyCode.A)) && (rb.velocity[2] <= maxVelocity) )
+        if (rb.velocity[0] <= maxVelocity)
         {
-            rb.AddForce(0,0,sidewaysForce);
+            rb.AddForce(forwardForce, 0, 0);
         }
-        if ( (Input.GetKey(UnityEngine.KeyCode.D)) && (rb.velocity[2] >= (-1*maxVelocity)) )
-        {
-            rb.AddForce(0,0,-1 * sidewaysForce);
-        }
-        if ( (Input.GetKey(UnityEngine.KeyCode.W)) && (rb.velocity[0] <= maxVelocity) )
-        {
-            rb.AddForce(forwardForce,0,0);
-        }
-        if ( (Input.GetKey(UnityEngine.KeyCode.S)) && (rb.velocity[0] >= (-1*maxVelocity)) )
-        {
-            rb.AddForce(-1 * forwardForce,0,0);
-        }//ground is at y=0
+
+        // Ground is at y=0
         if ((Input.GetKey(UnityEngine.KeyCode.Space)) && rb.position[1] < 0.1 && rb.velocity[1] <= 0)
         {
-            rb.AddForce(0, 5*upwardForce, 0);
+            rb.AddForce(0, 5 * upwardForce, 0);
+        }
+
+        if ((Input.GetKey(UnityEngine.KeyCode.A)) && (rb.velocity[2] <= maxVelocity))
+        {
+            rb.AddForce(0, 0, sidewaysForce);
+        }
+
+        if ((Input.GetKey(UnityEngine.KeyCode.D)) && (rb.velocity[2] >= (-1 * maxVelocity)))
+        {
+            rb.AddForce(0, 0, -1 * sidewaysForce);
         }
     }
 }
---
+++ lastlight/Assets/ScoreManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour
+{
+    int score;
+    public int Score { get => score; }
+
+    void Update()
+    {
+        score += 1;
+    }
+}
