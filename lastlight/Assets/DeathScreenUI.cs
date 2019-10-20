dd6a0a954b15fe4e8457fa8dbdecebb955c046da
Jordan Peters <rollersteaam@googlemail.com>
2019-10-20 02:39:05 +0100

    Add death screen

---
+++ lastlight/Assets/DeathScreenUI.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DeathScreenUI : MonoBehaviour
+{
+    CanvasGroup group;
+
+    void Start()
+    {
+        group = GetComponent<CanvasGroup>();
+
+        Player.OnPlayerDie += (o, e) =>
+        {
+            Display();
+        };
+    }
+
+    void Display()
+    {
+        group.alpha = 1;
+        group.interactable = true;
+        group.blocksRaycasts = true;
+    }
+
+    public void TryAgain()
+    {
+        ProgressionManager.Instance.TryAgain();
+    }
+}
