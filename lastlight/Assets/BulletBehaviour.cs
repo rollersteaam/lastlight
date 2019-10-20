b081c7a13e8fc25b7e2e6c1712c397661ede76e5
Milo Higgins <mh882@exeter.ac.uk>
2019-10-20 12:41:32 +0100

    Add ranged enemy behaviour

---
+++ lastlight/Assets/BulletBehaviour.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletBehaviour : MonoBehaviour
+{
+    Rigidbody rb;
+    public float speed = 0.5f;
+    public float forwardForce = 100f;
+    GameObject player;
+    Vector3 target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        player = GameObject.FindWithTag("Player");
+        rb.rotation = new Quaternion(0.7071068f, 0, 0, 0.7071068f);
+        rb.transform.LookAt(player.transform);
+        rb.transform.position = GameObject.FindWithTag("Enemy").transform.position;
+        target = new Vector3(player.transform.position.x,player.transform.position.y,player.transform.position.z);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.transform.position = Vector3.MoveTowards(transform.position, target, speed);
+        if (transform.position == target)
+        {
+        Destroy(gameObject);//bullet has missed player
+        }
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject == player)
+        {
+            player.GetComponent<Player>().Damage(10);
+            Destroy(gameObject);
+        }
+        
+    }
+}
--- lastlight/Assets/RangedEnemyBehaviour.cs
+++ lastlight/Assets/RangedEnemyBehaviour.cs
@@ -6,16 +6,38 @@ public class RangedEnemyBehaviour : MonoBehaviour
 {
     GameObject player;
     Rigidbody enemy;
+    public int argoDistance = 30;
+    public GameObject bullet;//set in inspector
+    bool fired;
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.FindWithTag("Player");
         enemy = GetComponent<Rigidbody>();
+        fired = false;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        
+        if ( (fired == false) && (Mathf.Abs(player.transform.position.x - transform.position.x) <= argoDistance) )
+        {
+            fired = true;
+            Chrono.Instance.After(3,() =>
+             {
+                 GameObject clone;
+                 clone = Instantiate(bullet, enemy.transform.position, Quaternion.identity);
+                 clone.transform.LookAt(player.transform);
+                 fired = false;
+             });
+        }
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject == player)
+        {
+            player.GetComponent<Player>().Damage(20);
+            Destroy(gameObject);
+        }
     }
 }
